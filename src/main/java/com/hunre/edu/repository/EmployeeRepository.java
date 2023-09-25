6a75d465b6c57b93451160e9f98d751a7f0d83f4
Trinh Minh Duc <102575050+minhducc2003@users.noreply.github.com>
2023-09-25 19:24:31 +0700

    Add files via upload

---
+++ src/main/java/com/hunre/edu/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.hunre.edu.repository;
+
+import com.hunre.edu.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
